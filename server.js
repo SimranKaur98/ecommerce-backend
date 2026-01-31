3960698380d2bca6b340ba128dc7d5a5064a856f
Simran KAur <simrankaurphulldevgan@gmail.com>
2026-01-31 23:44:56 +0530

    cors origin set to localhost

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 
 // Serve images from the images folder
@@ -103,3 +106,5 @@ if (productCount === 0) {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
